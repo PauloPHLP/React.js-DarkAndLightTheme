33e711d6fcd37fa780973ecea9b9cfa5fcdd786a
Paulo <paulo.phlp@outlook.com>
2020-12-11 23:42:57 -0300

    Chore: Add project into the repository.

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+const dark ={
+  title: 'dark',
+  colors: {
+    primary: '#333333',
+    secundary: '#c62e65',
+    background: '#222222',
+    text: '#ffffff'
+  },
+};
+
+export default dark;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+const light ={
+  title: 'light',
+  colors: {
+    primary: '#c62e65',
+    secundary: '#d63af9',
+    background: '#f5f5f5',
+    text: '#33333'
+  },
+};
+
+export default light;
